3764002dafc8c481bf01449b0a0994a488bb6100
hazy-1 <hajime2203@gmail.com>
2023-12-18 05:13:52 +0900

    Remove unnecessary code

--- speechToText.py
+++ speechToText.py
@@ -9,14 +9,6 @@ transcript = client.audio.transcriptions.create(
     response_format="text"
 )
 
-# translation = client.chat.completions.create(
-#     model="gpt-4-1106-preview",
-#     messages=[
-#         {"role": "user", "content": "こんにちは。API呼び出しのテストです。何か発言お願いします。"}
-#     ],
-#     response_format="text"
-# )
-
 with open("./output/output2.txt", "w") as file:
     for sentence in transcript.split('.'):
         file.write(sentence.strip() + ".\n")
